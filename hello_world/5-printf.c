75afda60a6b2286a5e899e9eaa333aaeb419c902
alexeychern0v <alexchernov063@gmail.com>
2023-10-11 09:24:21 -0700

    c

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+/**
+ * main - This is the entry point of the code
+ *
+ * Return - 0 Successful
+ */
 int main(void)
 {
 printf("With proper grammar, but the outcome is a piece of art,\n");
