2036d1e292d21d5199af8ab1e0fc12a41a436fd1
alexeychern0v <alexchernov063@gmail.com>
2023-10-16 04:43:29 -0700

    if

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char az = 'a';
+
+	for (az = 'a'; az < 'z'; az++)
+	{
+		if (az != 'e' && az != 'q')
+			putchar(az);
+	}
+putchar('\n');
+return (0);
+}
