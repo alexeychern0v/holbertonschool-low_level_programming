60a05e72e84d55f8f6d5983303f69539b629341b
alexeychern0v <alexchernov063@gmail.com>
2023-10-16 05:41:29 -0700

    forif

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char digit = '0';
+	char af = 'a';
+	for (digit = '0'; digit <= '9'; digit++)
+		putchar(digit);
+	for (af = 'a'; af <= 'f'; af++)
+		putchar(af);
+	putchar('\n');
+	return (0);
+}
