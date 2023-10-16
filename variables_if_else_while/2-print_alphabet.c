5b7d1ac5ee5e3bd44a4c2aa01be91744ee566c5c
alexeychern0v <alexchernov063@gmail.com>
2023-10-16 04:18:21 -0700

    if

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char az = 'a';
+	for (az = 'a'; az <= 'z'; az++)
+		putchar(az);
+	return (0);
+}
