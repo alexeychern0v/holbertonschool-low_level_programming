a683874672bd5bfa614832596a993945b349952a
alexeychern0v <alexchernov063@gmail.com>
2023-10-17 03:45:08 -0700

    loops

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include"main.h"
+/**
+ * print_alphabet_x10 - use of prototype
+ *
+ * 
+ */
+void print_alphabet_x10(void)
+{
+	int k = 0;
+	char az = 'a';
+
+	while (k <= 10)
+	{
+		for (az = 'a'; az <= 'z'; az++)
+			_putchar(az);
+		_putchar('\n');
+		k++;
+	}
+}
