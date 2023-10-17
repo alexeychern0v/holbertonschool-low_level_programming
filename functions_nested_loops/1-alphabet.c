b9d34ac1919c6a68d5db7310ba8a5b13f18db2c6
alexeychern0v <alexchernov063@gmail.com>
2023-10-17 03:25:58 -0700

    loops

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,7 +1,14 @@
 #include "main.h"
-
-int main(void)
+/**
+ * print_alphabet - use of prototype
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
 {
-    print_alphabet();
-    return (0);
+	char az = 'a';
+
+	for (az = 'a'; az <= 'z'; az++)
+		_putchar(az);
+	_putchar('\n');
 }
