4cf9c6d7e5ad9fed3c8f02f59aa1485faf00ad61
alexeychern0v <alexchernov063@gmail.com>
2023-10-17 11:10:19 -0700

    loops

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _abs - find the absolute value of int
+ *
+ *@k: number
+ *
+ * Return: 0 succes
+ */
+int _abs(int k)
+{
+	if (k > 0)
+		return (k);
+	else if (k < 0)
+	{
+		int abs_k;
+
+		return (abs_k);
+	}
+	else
+	{
+		int zero_k;
+
+		return (zero_k);
+	}
+	return (0);
+}
