e532e4d6f45c6332bc118530ea50f29f3793ce11
alexeychern0v <alexchernov063@gmail.com>
2023-10-17 11:27:38 -0700

    loops

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_last_digit - find last digit of number
+ *
+ * @m: number to check
+ *
+ * Return: last digit
+ */
+int print_last_digit(int m)
+{
+	int last;
+
+	last = m % 10;
+	return (last);
+}
