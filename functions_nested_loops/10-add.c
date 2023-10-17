62f9c1ad8f48ebcd171500579a7cfb12ceef8a39
alexeychern0v <alexchernov063@gmail.com>
2023-10-17 11:45:38 -0700

    loops

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - find the sum of two numbers
+ *
+ *@a: number
+ *
+ *@b: number
+ *
+ * Return: sum
+ */
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+	return (c);
+}
