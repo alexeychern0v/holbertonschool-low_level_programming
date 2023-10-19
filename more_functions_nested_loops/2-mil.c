323fc34c3945ff3740230bc1095d01756920e006
alexeychern0v <alexchernov063@gmail.com>
2023-10-19 04:00:34 -0700

    more

---
+++ more_functions_nested_loops/2-mil.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * mul - multiple two numbers
+ *
+ *@a: number
+ *
+ *@b: number
+ *
+ * Return: sum
+ */
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+	return (c);
+}
