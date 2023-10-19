a8e01612a1f77f2fadc37ff0e9d059dab6218fa8
alexeychern0v <alexchernov063@gmail.com>
2023-10-19 02:25:37 -0700

    loops

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -12,7 +12,7 @@ void print_to_98(int n)
 
 	if (n <= 98)
 	{
-		for (n ; n < 99; n++)
+		for (n=0 ; n < 99; n++)
 		{
 			if (n < 0)
 			{
@@ -46,7 +46,7 @@ void print_to_98(int n)
 	}
 	if (n > 99)
 	{
-		for (n; n >= 98; n--)
+		for (n = 0 ; n >= 98; n--)
 		{
 			if (n >= 100)
 			{
