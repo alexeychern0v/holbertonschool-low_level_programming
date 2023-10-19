fde14ab4b04e72c6e2ea1d75288773339b4905d7
alexeychern0v <alexchernov063@gmail.com>
2023-10-19 06:00:44 -0700

    more

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * print_square - print square
+ *
+ *@size: square size
+ *
+ * Return: Always 0 (Success)
+ */
+void print_square(int size)
+{
+	int n = 0;
+	int m = 0;
+
+	if (size > 0)
+	{
+		for (n = 0; n < size; n++)
+		{
+			for (m = 0; m < size; m++)
+			{
+				_putchar(35);
+			}
+
+		}
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
