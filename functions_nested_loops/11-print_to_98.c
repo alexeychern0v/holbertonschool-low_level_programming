54901795c1bd0679164b4f087f97b0a81613f382
alexeychern0v <alexchernov063@gmail.com>
2023-10-19 04:37:18 -0700

    more

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,3 +1,4 @@
+#include "main.h"
 #include <stdio.h>
 /**
  * main - Entry point
@@ -6,36 +7,32 @@
  *
  * Return: Always 0 (Success)
  */
-int main()
+void print_to_98(int n)
 {
-    int n = 98;
-    if (n < 98)
-    {
-        for (; n < 99; n++)
-                {
-                    printf("%d", n);
-                    if (n != 98)
-                    {
-                        printf(", ");
-                    }
-                }
-    }
-    
-    else if (n > 98)
-    {
-        for (; n > 97; n--)
-        {
-            printf("%d", n);
-            if (n != 98)
-            {
-                printf(", ");
-            }
-            
-        }
-    }
-    else
-    {
-        printf("98");
-    }
-    return (0);
+	if (n < 98)
+	{
+		for (; n < 99; n++)
+		{
+			printf("%d", n);
+			if (n != 98)
+			{
+				printf(", ");
+			}
+		}
+	}
+	else if (n > 98)
+	{
+		for (; n > 97; n--)
+		{
+			printf("%d", n);
+			if (n != 98)
+			{
+				printf(", ");
+			}
+		}
+	}
+	else
+	{
+		printf("98");
+	}
 }
