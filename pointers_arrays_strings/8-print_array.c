a44f2a62122135d98a3f041cecb6ee3defef1b1c
alexeychern0v <alexchernov063@gmail.com>
2023-10-24 14:47:32 -0700

    pointers

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - print n elements of array
+ *
+ * @a: array to print
+ *
+ * @n: number of characters to print
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i != (n - 1))
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+}
