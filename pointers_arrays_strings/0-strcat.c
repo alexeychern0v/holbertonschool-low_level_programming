a4b7ba9c1a9b63eaf45b35259ba95568ddcfe549
alexeychern0v <alexchernov063@gmail.com>
2023-10-25 05:20:57 -0700

    pointers

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strcpy -  copy a string
+ *
+ * @dest: destinations
+ *
+ * @src: source
+ *
+ * Return:  pointer to dest
+ */
+char *_strcat(char *dest, char *src);
+{
+	int len = 0;
+	int j;
+
+	while (dest[len] != '\0')
+	{
+		len++;
+	}
+	for (j = 0; src[j] != '\0'; j++; len++)
+	{
+		dest[len] = src [j];
+	}
+	dest[len] = '\0';
+	return (*dest);
+}
