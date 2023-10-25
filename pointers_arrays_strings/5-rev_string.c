6b6a4fb42d1c253049ef77f0aa07b1b62e5250b5
alexeychern0v <alexchernov063@gmail.com>
2023-10-25 02:20:05 -0700

    pointers

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * rev_string - print string in reverse
+ *
+ * @s: string to print
+ */
+void rev_string(char *s)
+{
+	int len = 0;
+	int i, temp;
+	
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+	for (i = 0; i < len; len++)
+	{
+		temp = s[i];
+		s[i] = s[len - i - 1];
+		s[i] = temp;
+	}
+	_putchar('\n');
+}
+
