250f919418b883bedcf2d20889c4fa46b5db6949
alexeychern0v <alexchernov063@gmail.com>
2023-10-25 05:23:23 -0700

    pointers

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -12,15 +12,17 @@
 char *_strcat(char *dest, char *src)
 {
 	int len = 0;
-	int j;
+	int j = 0;
 
 	while (dest[len] != '\0')
 	{
 		len++;
 	}
-	for (j = 0; src[j] != '\0'; j++; len++)
+	while (src[j] != '\0')
 	{
 		dest[len] = src [j];
+		j++;
+		len++;
 	}
 	dest[len] = '\0';
 	return (*dest);
