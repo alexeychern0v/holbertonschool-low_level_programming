b71bad92facf6802fe9ba52fd85f7256fa106206
alexeychern0v <alexchernov063@gmail.com>
2023-10-29 14:17:29 -0700

    pointers

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,11 @@
+#include "main.c"
+
+int _strcmp(char *s1, char *s2)
+{
+    while (*s1 != '\n' && *s2 != '\n' && (*s1 == *s2))
+    {
+        s1++;
+        s2++;
+    }
+    return (*s1 - *s2);
+}
