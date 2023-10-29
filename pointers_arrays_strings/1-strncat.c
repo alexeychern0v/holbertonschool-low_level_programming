2bca13c2f43bff8f8e9f34210f4dfbb29195d281
alexeychern0v <alexchernov063@gmail.com>
2023-10-29 08:56:07 -0700

    pointers

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+  * _strncat - concatenates two strings
+  *
+  * @dest: destination
+  *
+  * @src: source
+  *
+  * @n: limit
+  *
+  * Return: a poiner to dest
+  */
+char *_strncat(char *dest, char *src, int n)
+{
+	int len = 0, j = 0;
+
+	while (dest[len] != '\n')
+	{
+		len++;
+	}
+	while (j < n && src[j])
+	{
+		dest[len] = src[j];
+		len++;
+		j++;
+	}
+	dest[len + n + 1] = '\0';
+	return (dest);
+}
