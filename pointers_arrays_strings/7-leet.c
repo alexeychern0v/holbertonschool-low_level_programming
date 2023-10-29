04a1048113640ba7907772f52a178e55583fae9f
alexeychern0v <alexchernov063@gmail.com>
2023-10-29 15:29:43 -0700

    pointers

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -12,15 +12,16 @@ char *leet(char *s)
 	char a[] = { 'a', 'e', 'o', 't', 'l' };
 	char n[] = { 4, 3, 0, 7, 1 };
 	int i = 0;
+	int len = 0;
 
-	while (s != '\0')
+	while (s[len] != '\0')
 	{
 		for (i = 0; i < 5; i++)
 		{
-			if (s == a[i] || s == a[i] - 32)
-				s = n[i] + '0';
+			if (s[len] == a[i] || s[len] == a[i] - 32)
+				s[len] = n[i] + '0';
 		}
-		s++;
+		len++;
 	}
 	return (r);
 }
