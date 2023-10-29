0be5563de3e0a3275e625a78bdc713a440a51034
alexeychern0v <alexchernov063@gmail.com>
2023-10-29 15:14:03 -0700

    pointers

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -10,10 +10,11 @@ void rev_string(char *s)
 	int len = 0, a = 0;
 	char temp;
 
-	while (s[len++])
+	while (s[len] != '\0')
 	{
 		len++;
 	}
+	a = len;
 	for (a = len - 1; a >= (len / 2); a--)
 	{
 		temp = s[a];
