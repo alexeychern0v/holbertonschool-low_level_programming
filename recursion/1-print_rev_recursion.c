5e81125bddd8bc5504a8e6bca3397ef01fe75342
alexeychern0v <alexchernov063@gmail.com>
2023-10-30 05:12:32 -0700

    recursion

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *_print_rev_recursion - print reverse of a  string 
+ *
+ *@s: string to print
+ */
+void _print_rev_recursion(char *s)
+{
+    if (*s != '\0')
+    {
+        _print_rev_recursion(s + 1);
+        _putchar(*s);
+    }
+}
--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -16,4 +16,5 @@ int _strlen_recursion(char *s)
 	{
 		return (1 + _strlen_recursion(s + 1));
 	}
+	_putchar('\n');
 }
