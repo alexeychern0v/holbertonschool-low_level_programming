b0a72785385e284f63374b7d53fc2c69b3b0adbb
alexeychern0v <alexchernov063@gmail.com>
2023-11-03 12:39:31 -0700

    argc

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - name of file
+ *
+ * @argc: The number of arguments supplied to the program
+ *
+ * @argv: An array of pointers to the arguments
+ *
+ * Return: 0 is successful
+ *
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
