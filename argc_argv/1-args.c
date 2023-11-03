9a90f143be886e88e6ab7f592224f0c9a750b619
alexeychern0v <alexchernov063@gmail.com>
2023-11-03 12:45:57 -0700

    argc

---
+++ argc_argv/1-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - number of args
+ *
+ * @argc: The number of arguments supplied to the program
+ *
+ * @argv: An array of pointers to the arguments
+ *
+ * Return: 0 is successful
+ *
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	argv = argv;
+	i = argc - 1;
+	printf("%d\n", i);
+	return (0);
+}
