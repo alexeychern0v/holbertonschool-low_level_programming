d3ba37cc618b7b1df61e3adfe49e0e39f7002d1a
alexeychern0v <alexchernov063@gmail.com>
2023-11-03 12:49:48 -0700

    argc

---
+++ argc_argv/2-args.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - print received args
+ *
+ * @argc: The number of arguments supplied to the program
+ *
+ * @argv: An array of pointers to the arguments
+ *
+ * Return: 0 is successful
+ *
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return(0);
+}
