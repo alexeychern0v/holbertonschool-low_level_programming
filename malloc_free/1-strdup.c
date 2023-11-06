efc6a58406b66bc697721304e6020ff0b10b45c7
alexeychern0v <alexchernov063@gmail.com>
2023-11-06 04:45:19 -0800

    malloc

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+char *_strdup(char *str)
+{
+	int size = 0;
+	int i;
+        char *a;
+
+	if (str == 0)
+	{
+		return (NULL);
+	}
+	while (str[size] != '\0')
+	{
+		size++;
+	}
+	a = malloc((size) * sizeof(char));
+	if (a == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i <= size; i++)
+	{
+		a[i] = str[i];
+	}
+	return (a);
+}
