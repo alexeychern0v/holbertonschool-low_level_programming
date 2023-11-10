c92bce0e9be9e8e05b44b6451db46e6e72a66b52
alexeychern0v <alexchernov063@gmail.com>
2023-11-10 08:58:48 -0800

    malloc

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -22,31 +22,19 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *s;
 
 	if (s1 == NULL)
-	{
 		return ("");
-	}
 	if (s2 == NULL)
-	{
 		return ("");
-	}
 	while (s1[size1] != '\0')
-	{
 		size1++;
-	}
 	while (s2[size2] != '\0')
-	{
 		size2++;
-	}
 	sizeall = size1 + size2 + 1;
 	s = malloc(sizeall * sizeof(char));
 	if (s == NULL)
-	{
 		return (NULL);
-	}
 	for (i = 0; i < size1; i++)
-	{
 		s[i] = s1[i];
-	}
 	if (n >= size2)
 	{
 		for (j = 0; j < size2; j++)
