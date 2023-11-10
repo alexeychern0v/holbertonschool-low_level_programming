6f4ddf08f086b49da469a06813fd996ac907812c
alexeychern0v <alexchernov063@gmail.com>
2023-11-10 14:01:32 -0800

    malloc

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -1,51 +1,42 @@
 #include "main.h"
+#include <stdlib.h>
+
 /**
-  *str_concat - concatenates two strings
-  *
-  *@s1: string
-  *
-  *@s2: string
-  *
-  *Return: concatenated strings
-  */
+ * str_concat - concatenate two strings
+ *
+ * @s1: char
+ *
+ * @s2: char
+ *
+ * Return: string
+ */
+
 char *str_concat(char *s1, char *s2)
 {
 	int size1 = 0;
 	int size2 = 0;
-	int sizeall = 0;
-	int i, j;
-	char *s;
+	int i;
+	char *r;
 
 	if (s1 == NULL)
 	{
-		return ("");
+		s1 = "";
 	}
 	if (s2 == NULL)
 	{
-		return ("");
+		s2 = "";
 	}
 	while (s1[size1] != '\0')
-	{
 		size1++;
-	}
 	while (s2[size2] != '\0')
-	{
 		size2++;
-	}
-	sizeall = size1 + size2 + 1;
-	s = malloc(sizeall * sizeof(char));
-	if (s == NULL)
-	{
+	r = (char *)malloc(size1 + size2 + 1);
+	if (r == NULL)
 		return (NULL);
-	}
 	for (i = 0; i < size1; i++)
-	{
-		s[i] = s1[i];
-	}
-	for (j = 0; j < size2; j++)
-	{
-		s[i + j] = s2[j];
-	}
-	s[i + j] = '\0';
-	return (s);
+		r[i] = s1[i];
+	for (i = 0; i < size2; i++)
+		r[size1 + i] = s2[i];
+	r[size1 + size2] = '\0';
+	return (r);
 }
