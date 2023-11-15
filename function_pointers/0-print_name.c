03fda567315c2a9cf4fed55d029768c1f2fc4d46
alexeychern0v <alexchernov063@gmail.com>
2023-11-15 07:12:41 -0800

    func_pointer

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * print_name - print name of function
+ * @name : name of function
+ * @f: pointer
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
