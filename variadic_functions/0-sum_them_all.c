085939b865a0a9007a67a1a036cf88fffb227d1f
alexeychern0v <alexchernov063@gmail.com>
2023-11-16 04:18:19 -0800

    variadic

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,16 @@
+#include <variadic_functions.h>
+
+int sum_them_all(const unsigned int n, ...)
+{
+	int sum = 0;
+	int i;
+
+	if (n == 0)
+		return (0);
+	va_list ptr;
+	va_start(ptr, n);
+	for (i = 0; i < n; i++)
+		sum += va_arg(ptr, int);
+	va_end(ptr);
+	return sum;
+}
