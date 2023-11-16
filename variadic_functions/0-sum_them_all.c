cec08ba9393674aa0284ac8f3dfddeae2b79ba83
alexeychern0v <alexchernov063@gmail.com>
2023-11-16 04:24:39 -0800

    variadic

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -1,4 +1,9 @@
-#include <variadic_functions.h>
+#include "variadic_functions.h"
+/**
+ * sum_them_all - sum of all parameters
+ *
+ * @n: n arguments
+ */
 
 int sum_them_all(const unsigned int n, ...)
 {
@@ -8,9 +13,11 @@ int sum_them_all(const unsigned int n, ...)
 	if (n == 0)
 		return (0);
 	va_list ptr;
+
 	va_start(ptr, n);
+
 	for (i = 0; i < n; i++)
 		sum += va_arg(ptr, int);
 	va_end(ptr);
-	return sum;
+	return (sum);
 }
