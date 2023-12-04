abf4f0a294d72d6686a0d247a4b2d0c792462ff8
alexeychern0v <alexchernov063@gmail.com>
2023-12-04 02:49:37 -0800

    first task, main added

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ *
+ *
+ *
+ *
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int f;
+
+	if (filename == NULL)
+		return (0);
+	f = fopen (filename, O_RDONLY);
+	if (fd = -1)
+		return (0);
+
+
+
+
+
+
+
+
+
+}
