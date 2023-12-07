b55fffe1b02cd5317a2b1807cf3265f9f3e2a372
alexeychern0v <alexchernov063@gmail.com>
2023-12-07 03:03:58 -0800

    task 0

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -9,20 +9,24 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int f;
+	int fd;
+	ssize_t numwr, numrd;
+	char *buff;
 
 	if (filename == NULL)
 		return (0);
-	f = fopen (filename, O_RDONLY);
-	if (fd = -1)
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	buff = malloc(sizeof(char) * letters);
+	if (buff == NULL)
 		return (0);
 
+	numrd = read(fd, buff, letters);
+	numwr = write(STDOUT_FILENO, buff, numrd);
 
+	close(fd);
+	free(buff);
 
-
-
-
-
-
-
+	return (numwr);
 }
