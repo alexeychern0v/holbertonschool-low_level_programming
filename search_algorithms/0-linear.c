62d1b8333a7d7d0d0b7abf9e1a36a520aa52e3da
Alexey Chernov <alexchernov063@gmail.com>
2024-01-19 08:50:23 -0800

    added linear

---
+++ search_algorithms/0-linear.c
@@ -0,0 +1,20 @@
+#include "search_algorithms.h"
+
+/**
+ *
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+	size_t index;
+
+	for (index = 0; index < size; index++)
+	{
+		printf("Value checked array[%lu] = [%d]\n", index, array[index]);
+		if (array[index] == value)
+		{
+			return (index);
+		}
+	}
+	return (-1);
+}
